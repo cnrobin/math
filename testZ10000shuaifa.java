003bf0ad8d52602d051019c708cff52ee74a101e
cnrobin <robintan@126.com>
2019-10-05 20:41:21 +0800

    testZ10000shuaifa
    testZ10000shuaifa  description

---
+++ testZ10000shuaifa.java
@@ -0,0 +1,40 @@
+package robin.math;
+import java.util.*;
+public class testZ10000shuaifa{
+	public static void main(String args[]){
+		GetDBData g=new GetDBData();
+		List list2=g.get_prime();
+		g.close();
+		System.out.println("list2="+list2);
+		int prime_n=list2.size();
+		int prime_array[]=new int[prime_n];
+		int prime_array_static[]=new int[prime_n];
+		for (int i=0;i<prime_n;i++)
+		{
+			prime_array[i]=(int)list2.get(i);
+			prime_array_static[i]=0;
+		}
+		for( int i=0;i<prime_n;i++){
+			System.out.println("prime["+i+"]="+prime_array[i]);
+		}
+		int z_array[]=new int[10001];
+		for(int i=0;i<=10000;i++){
+			z_array[i]=i;
+		}
+		for(int i=4;i<=10000;i++){
+			for(int j=1;j<prime_n;j++){
+				if(z_array[i]%prime_array[j]==0){
+					z_array[i]=prime_array[j];
+					prime_array_static[j]++;
+					break;
+				}
+			}
+		}
+		for(int i=1;i<10000;i++){
+			System.out.println("z_array["+i+"]="+z_array[i]);
+		}
+		for(int i=1;i<prime_n;i++){
+			System.out.println("prime_array_static["+i+"]="+prime_array_static[i]);
+		}
+	}
+}
